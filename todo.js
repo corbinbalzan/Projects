6a44d4c552ccef7ee9e28e73bd369ae335611f35
corbinbalzan <corbinbalzan@gmail.com>
2017-07-05 14:55:25 -0400

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,192 @@
+// Main Goal: Add interactivity to the template
+
+// Create a list to hold the Completed
+// List for the incomplete 
+var inputTask = document.getElementById("addItem");
+var addButton = document.getElementsByTagName("button")[0];
+var incomplete = document.getElementById("incomplete");
+var complete = document.getElementById("complete");
+
+
+
+// Create a new item for the incomplete upon pressing Add
+
+var createTask = function(task){
+
+	//create each part of added item
+	var listItem = document.createElement("li");
+	var inputCheck = document.createElement("input");
+	var label = document.createElement("label");
+	var buttonEdit = document.createElement("button");
+	var buttonDelete = document.createElement("button");
+	var editInput = document.createElement("input");
+
+	// Modify the types of each item 
+
+	inputCheck.type = "checkbox";
+	editInput.type = "text";
+
+	buttonEdit.innerText = "Edit";
+	buttonDelete.innerText = "Delete";
+	buttonEdit.className = "edit";
+	buttonEdit.id = 'editt';
+	buttonDelete.className = "delete";
+	editInput.className = "editor"
+	editInput.id = 'edittor'
+	// Make the label of this new item the input value
+
+	label.innerText = task; 
+	editInput.value = task;
+
+	listItem.appendChild(inputCheck);
+	listItem.appendChild(label);
+	listItem.appendChild(editInput);
+	listItem.appendChild(buttonEdit);
+	listItem.appendChild(buttonDelete);
+    
+  
+    editInput.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+        buttonEdit.click();
+    }
+});
+      
+	return listItem;
+
+
+};
+
+// Add the text to the incomplete 
+
+var addItem = function(){
+	var value1 = createTask(inputTask.value);
+	console.log(value1);
+	incomplete.appendChild(value1);
+	inputTask.value = "";
+	TaskEvents(value1, taskCompleted);
+};
+
+addButton.addEventListener("click", addItem);
+
+
+// Move checked items to the complete section 
+
+var taskCompleted = function() {
+  var listItem = this.parentNode;
+  listItem.className += "complete1";
+  complete.appendChild(listItem);
+  TaskEvents(listItem, taskIncomplete);
+
+}
+
+
+
+// Edit the text when pressing the edit button 
+
+var editTask = function() {
+
+  var listItem = this.parentNode;
+  
+  var editInput = listItem.querySelector("input[type=text]")
+  var label = listItem.querySelector("label");
+  
+  var containsClass = listItem.classList.contains("editMode");
+    //if the class of the parent is .editMode 
+  if(containsClass) {
+      //switch from .editMode 
+      //Make label text become the input's value
+    label.innerText = editInput.value;
+  } else {
+      //Switch to .editMode
+      //input value becomes the label's text
+    editInput.value = label.innerText;
+  }
+  
+    // Toggle .editMode on the parent
+  listItem.classList.toggle("editMode");
+ 
+}
+
+
+// Delete the item when pressing the Delete button 
+
+var deleteTask = function(){
+	var lItem = this.parentNode;
+	var ul = lItem.parentNode;
+	ul.removeChild(lItem);
+};
+
+// Mark a task as incompleted
+var taskIncomplete = function() {
+  var listItem = this.parentNode;
+  incomplete.appendChild(listItem);
+  TaskEvents(listItem, taskCompleted);
+}
+
+
+var TaskEvents = function(taskItem, checkBoxEventHandler) {
+  //select children
+  var checkBox = taskItem.querySelector("input[type=checkbox]");
+  var editButton = taskItem.querySelector("button.edit");
+  var deleteButton = taskItem.querySelector("button.delete");
+  
+  //click triggers
+  editButton.onclick = editTask;  
+  deleteButton.onclick = deleteTask;
+  checkBox.onchange = checkBoxEventHandler;
+/*  
+  editButton.addEventListener('keyup', function() {
+      event.preventDefault();
+      if (event.keyCode == 13)){
+        editTask;
+      }
+  
+  var buttons = document.getElementsByTagName('input');
+  
+  for (var i=0; i < buttons.length; i++) {
+    buttons[i].addEventListener('keyup', function() {
+      event.preventDefault();
+      if (event.keyCode == 13) {
+          document.getElementsByClassName("edit").click();
+          editButton[i].addEventListener('click', function() {
+          alert(this.textContent);
+      }
+    });
+  }
+*/
+}
+
+document.getElementById("addItem")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+        document.getElementById("addButton1").click();
+    }
+});
+
+
+document.getElementsByTagName("input")[2]
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+        document.getElementsByTagName("input")[3].click();
+    }
+});
+
+
+   
+/*
+var buttons = document.getElementsByClassName('editor');
+
+for (var i=0; i < buttons.length; i++) {
+  buttons[i].addEventListener('keyup', function() {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+        document.getElementByClassName("edit").click();
+        editTask;
+    };
+  });
+}
+
+*/
